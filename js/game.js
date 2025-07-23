0ce52b36b4cda2afccf55a3ef5b27e10d86fe12d
Steve O <steveokeefe88@gmail.com>
2025-07-23 00:02:00 -0230

    Update game.js so that the square is controlled by the user used WASD, moves faster and stays on the canvas

--- js/game.js
+++ js/game.js
@@ -3,18 +3,38 @@ const context = canvas.getContext('2d');
 
 let rectX = 100;
 let rectY = 100;
-let rectSpeed = 1;
 const rectWidth = 50;
 const rectHeight = 50;
 
+let keys = {
+    w: false,
+    a: false,
+    s: false,
+    d: false,
+};
+
+document.addEventListener('keydown', (event) => {
+    keys[event.key] = true;
+});
+
+document.addEventListener('keyup', (event) => {
+    keys[event.key] = false;
+});
+
 const gameLoop = () => {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    if (rectX + rectWidth > canvas.width) {
-        rectSpeed = -1;
-    } else if (rectX < 0) {
-        rectSpeed = 1;
+    if (keys.w && rectY > 0) {
+        rectY -= 5;
     }
-    rectX += rectSpeed;
+    if (keys.a && rectX > 0) {
+        rectX -= 5;
+    }
+    if (keys.s && rectY + rectHeight < canvas.height) {
+        rectY += 5;
+    }
+    if (keys.d && rectX + rectWidth < canvas.width) {
+        rectX += 5;
+    }
     context.fillRect(rectX, rectY, rectWidth, rectHeight);
     requestAnimationFrame(gameLoop);
 };
