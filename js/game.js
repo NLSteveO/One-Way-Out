039c110249fa9c1d08682b1a9c12ba4d0f1d2829
Steve O <steveokeefe88@gmail.com>
2025-07-22 23:43:41 -0230

    Update game.js so that the square bounces off the edges and changes direction

--- js/game.js
+++ js/game.js
@@ -3,12 +3,18 @@ const context = canvas.getContext('2d');
 
 let rectX = 100;
 let rectY = 100;
+let rectSpeed = 1;
 const rectWidth = 50;
 const rectHeight = 50;
 
 const gameLoop = () => {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    rectX += 1;
+    if (rectX + rectWidth > canvas.width) {
+        rectSpeed = -1;
+    } else if (rectX < 0) {
+        rectSpeed = 1;
+    }
+    rectX += rectSpeed;
     context.fillRect(rectX, rectY, rectWidth, rectHeight);
     requestAnimationFrame(gameLoop);
 };
