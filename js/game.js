13b2734f1d179cbf8b9727f2b67806f13f8ee907
Steve O <steveokeefe88@gmail.com>
2025-07-22 23:03:23 -0230

    Create game.js and draw black square oncanvas

---
+++ js/game.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById("gameCanvas");
+const context = canvas.getContext('2d');
+
+let rectX = 100;
+let rectY = 100;
+const rectWidth = 50;
+const rectHeight = 50;
+
+context.fillRect(rectX, rectY, rectWidth, rectHeight);
